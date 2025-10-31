d4fc2c139e2ec36c956708bf526132614b62bc5d
tristanpwdennis <tristanpwdennis@gmail.com>
2025-10-31 17:38:17 +1000

    add config for GCP and local data paths

---
+++ src/cairn/__init__.py
@@ -0,0 +1,7 @@
+"""
+cairn: a small stack of utility functions for loading, querying, and analysing Zarr arrays of genomic data.
+"""
+
+__version__ = "0.1.0"
+
+from . import io, pca  # convenience imports
